9aef186e63d8f710566d3e5a6d5a4807bb7da050
Deeptanshu ______ <109023764+thisisdeeptanshu@users.noreply.github.com>
2022-07-21 12:53:03 +0530

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,26 +1,11 @@
 try {
-document.getElementById("links-right").style.marginTop = (document.body.clientHeight/2 - document.getElementById("link-right").offsetWidth/0.5) + "px";
-document.getElementById("links-right").style.marginRight = "8px";
-document.getElementById("links-left").style.marginTop = (document.body.clientHeight/2 - document.getElementById("link-left").offsetWidth*2.75) + "px";
-document.getElementById("links-left").style.marginLeft = "8px";
+    document.getElementById("links-right").style.marginTop = (document.body.clientHeight/2 - document.getElementById("link-right").offsetWidth/0.5) + "px";
+    document.getElementById("links-right").style.marginRight = "8px";
+    document.getElementById("links-left").style.marginTop = (document.body.clientHeight/2 - document.getElementById("link-left").offsetWidth*2.75) + "px";
+    document.getElementById("links-left").style.marginLeft = "8px";
+    document.getElementById("links-normal").style.marginLeft = (document.body.clientWidth/2 - document.getElementById("links-normal").offsetHeight*2.25) + "px";
+    document.getElementById("links-normal").style.marginBottom = "18px";
 } catch {}
-// let total = 3;
-// let isSet = false;
-// function set() {
-//     if (isSet == false) {
-//         let selected = Math.round(Math.random()*3)
-//         for (let i = 0; i < total; i++) {
-//             if (i != selected) document.getElementById(`name${i+1}`).style.display = "none";
-//         }
-//         isSet = true;
-//     }
-// }
-// function reset() {
-//     for (let i = 0; i < total; i++) {
-//         document.getElementById(`name${i+1}`).style.display = "block";
-//     }
-//     isSet = false;
-// }
 
 let canCircleMove = true;
 
